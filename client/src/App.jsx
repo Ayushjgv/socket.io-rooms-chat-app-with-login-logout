2ff62a324681f1959c2170e53f0190da1d8babd1
Ayushjgv <ayushprajapat456@gmail.com>
2025-12-23 01:07:02 +0530

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,161 @@
+import React, { useMemo, useState } from 'react'
+import {io} from 'socket.io-client';
+import { useEffect } from 'react';
+
+const App = () => {
+
+  const socket=useMemo(()=>io('http://localhost:3000',{
+    withCredentials:true,
+  }),[]);
+  // const socket=io('http://localhost:3000');
+  const [Value, setValue] = useState('');
+  const [Room, setRoom] = useState('');
+  const [SocketID, setSocketID] = useState('');
+  const [Messages, setMessages] = useState([])
+  const [JoinRoom, setJoinRoom] = useState('');
+  const [User, setUser] = useState(null);
+  const [Username, setUsername] = useState('')
+
+
+
+
+
+
+  // console.log(Messages);  
+
+  useEffect(() => {
+    socket.on('connect', () => {
+      setSocketID(socket.id);
+      console.log('Connected to server with ID:', socket.id);
+    });
+
+    socket.on('welcome', (message) => {
+      console.log('Message from server:', message);
+    });
+
+    socket.on('receive_message',(msg)=>{
+      console.log('New message received:',msg);
+      setMessages((prevmsg)=>[...prevmsg,msg]);
+    });  
+
+    return()=>{
+      socket.disconnect();
+    };
+
+  }, [socket]);
+
+    
+
+async function loginhandler(){
+    // window.location.href='http://localhost:3000/login';
+    await fetch('http://localhost:3000/login',{
+      method:'GET',
+      credentials:'include'
+    });
+    // const data=await res.json();
+    // console.log('Login response:',data);
+
+
+    // if (!Username.trim()) {
+    //   alert('Username required');
+    //   return;
+    // }
+
+
+
+
+  //   await fetch('/login', {
+  //   method: 'GET',
+  //   credentials: 'include',
+  //   headers: { 'Content-Type': 'application/json' },
+  //   body: JSON.stringify({ Username }),
+  // });
+
+  // setUser(Username);
+  // socket.connect();
+}
+
+
+
+
+async function logouthandler(){
+    await fetch('http://localhost:3000/logout', {
+    method: 'GET',
+    credentials: 'include',
+  });
+
+  // socket.disconnect();
+  console.log('Logged out');
+  setUser(null);
+}
+
+
+
+  function handlesubmit(e){
+    e.preventDefault();
+    console.log('Sending message:',Value);
+    socket.emit('message',{msg:Value,Room:Room});
+    setValue('');
+  }
+
+
+
+  function JoinRoomHandler(e){
+    e.preventDefault();
+    if(JoinRoom!==''){
+      socket.emit('join_room',JoinRoom);
+      console.log(`Joining room: ${JoinRoom}`);
+      setRoom(JoinRoom);
+      setJoinRoom('');
+    }
+  }
+
+
+
+// console.log(socket.user.username);
+
+
+
+
+  return (
+    <div className="container">
+
+      <h4>Your Socket ID: {SocketID}</h4>
+      <form onSubmit={(e)=>{
+        e.preventDefault();
+      }}>
+          <input
+          type="text"
+          placeholder="Enter username"
+          value={Username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+
+        <button type='submit' onClick={loginhandler}>login</button>
+        <button type='submit' onClick={logouthandler}>logout</button>
+      </form>
+
+      <form onSubmit={JoinRoomHandler}>
+        <input type="text" value={JoinRoom} onChange={(e)=>setJoinRoom(e.target.value)} placeholder='Enter room name to join' />
+        <button type="submit">Join Room</button>
+      </form>
+
+      <form onSubmit={handlesubmit}>
+        <input value={Value} onChange={(e)=>setValue(e.target.value)} type="text" placeholder='Enter message' />
+        <input type="text" value={Room} onChange={(e)=>setRoom(e.target.value)} placeholder='Enter room name' />
+        <button type="submit">Send</button>
+      </form>
+
+
+      {Messages.map((m,i)=>{
+        return(
+          <div className="message">
+            {i}.{m}
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
+export default App
---
+++ server/server.js
@@ -0,0 +1,149 @@
+const express=require('express');
+const {Server}=require('socket.io');
+const http=require('http');
+const cors=require('cors');
+const jwt =require('jsonwebtoken');
+const cookieParser =require('cookie-parser');
+
+
+const app=express();
+const port=3000;
+const server=http.createServer(app);
+
+
+
+
+
+
+const secret="qwert"
+
+
+
+
+
+
+
+
+const io=new Server(server,{
+    cors:{
+        origin:'http://localhost:5173',
+        methods:['GET','POST'],
+        credentials:true
+    }
+});
+
+
+app.use(cors(
+    {
+        origin:'*',
+        methods:['GET','POST'],
+        credentials:true
+    }
+));
+
+
+const user=false;
+
+
+io.use((socket, next) => {
+
+    cookieParser()(socket.request, socket.request.res , (err) => {
+        if(err) return next(err);
+
+        const token = socket.request.cookies.token;
+        if(!token){
+            return next(new Error('Authentication error: Token not provided'));
+        }
+
+        const decoded =jwt.verify(token,secret);
+        console.log('Decoded token:',decoded);
+        // socket.user=decoded;
+        next();
+    });
+
+})
+
+
+
+
+
+
+
+
+
+
+io.on('connection',(socket)=>{
+    console.log('A user connected:',socket.id);
+    socket.emit('welcome',`Welcome to the Socket.io server! , ${socket.id}`);
+    socket.broadcast.emit('welcome',`Welcome to the Socket.io server! , ${socket.id}`);
+
+    socket.on('disconnect',()=>{
+        console.log('A user disconnected:',socket.id);
+    });
+
+
+    socket.on('message',(msg)=>{
+        console.log(`Message from ${socket.id}: ${msg.msg}`);
+        io.to(msg.Room).emit('receive_message', msg.msg);
+    });
+
+    socket.on('join_room',(room)=>{
+        socket.join(room);
+        console.log(`Socket ${socket.id} joined room: ${room}`);
+    });
+
+});
+
+
+
+
+
+app.get('/login',(req,res)=>{
+
+    // const {username} = req.body;
+
+    // const user = { id: Date.now(), username };
+
+    const token=jwt.sign({_id:"qwerfgth"},secret);
+
+    // const token = jwt.sign(
+    //     { userId: user.id, username: user.username },
+    //     secret,
+    //     { expiresIn: '1h' }
+    // );
+
+    res.cookie("token",token,{
+        httpOnly:true,
+        secure:true,
+        sameSite:'none',
+    });
+    res.json({message:"Login successful",token
+    });
+});
+
+app.get('/logout', (req, res) => {
+  res.clearCookie('token', {
+    httpOnly: true,
+    secure: true,
+    sameSite: 'none',
+  });
+
+  res.status(200).json({ message: 'Logged out successfully' });
+});
+
+
+
+
+
+
+
+app.get('/',(req,res)=>{
+    res.send('Hello, World!');
+});
+
+
+
+
+server.listen(port,()=>{
+    console.log(`Server is running on http://localhost:${port}`);
+});
